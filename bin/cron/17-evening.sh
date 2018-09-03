9b633f1a39b4baf086153006d51a24f05e3d7ed6
jobscale <jobscale@jsx.jp>
2018-09-03 10:14:54 +0900

    update cron

--- bin/cron/17-evening.sh
+++ bin/cron/17-evening.sh
@@ -2,19 +2,23 @@
 
 ~/bin/run-node ~/projects/node/speedtest-slack
 
-# 3日おき
-if [ $(expr \( $(date +%s) - $(date -d'2015/10/22' +%s) \) / 60 / 60 / 24 % 3) != 0 ]
-then
-    exit
-fi
+choseisan() {
+}
 
-~/bin/run-node ~/projects/node/itnews
-~/bin/run-node ~/projects/node/news
+day3() {
+  ~/bin/run-node ~/projects/node/itnews
+  ~/bin/run-node ~/projects/node/news
+}
+
+day6() {
+  ~/bin/run-node ~/projects/node/it
+}
+
+# 木曜日
+[ $(date '+%u') -eq 4 ] && choseisan
+
+# 3日おき
+[ $(expr \( $(date +%s) - $(date -d'2015/10/22' +%s) \) / 60 / 60 / 24 % 3) -eq 0 ] && day3
 
 # 6日おき
-if [ $(expr \( $(date +%s) - $(date -d'2015/10/22' +%s) \) / 60 / 60 / 24 % 6) != 0 ]
-then
-    exit
-fi
-
-~/bin/run-node ~/projects/node/it
+[ $(expr \( $(date +%s) - $(date -d'2015/10/22' +%s) \) / 60 / 60 / 24 % 6) -eq 0 ] && day6
