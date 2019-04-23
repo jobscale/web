9979012a4cc590f5a9e4b1e2b7d3a3c60d40a9b5
jobscale <jobscale@jsx.jp>
2019-04-24 07:50:52 +0900

    update dns

--- etc/bind/db.jsx
+++ etc/bind/db.jsx
@@ -1,39 +1,33 @@
 ;
 ; BIND data file for local loopback interface
 ;
-$TTL	60
-@	IN	SOA	jsx.jp. root.jsx.jp. (
-			      2		; Serial
-			 604800		; Refresh
-			  86400		; Retry
-			2419200		; Expire
-			 604800 )	; Negative Cache TTL
+$TTL 60
+@  IN  SOA  jsx.jp. root.jsx.jp. (
+           2   ; Serial
+      604800   ; Refresh
+       86400   ; Retry
+     2419200   ; Expire
+      604800 ) ; Negative Cache TTL
 ;
 
-@	IN	NS	ns1
-@	IN	NS	ns2
-@	IN	NS	ns3
+@       IN      NS      ns1
+@       IN      NS      ns2
+@       IN      NS      ns3
 aws     IN      NS      ns11.value-domain.com.
 aws     IN      NS      ns12.value-domain.com.
 aws     IN      NS      ns13.value-domain.com.
 
-@	IN	A	172.16.6.22
-
+@       IN      A       172.16.6.22
 *       IN      A       172.16.6.22
+
 ns1     IN      A       172.16.6.22
 ns2     IN      A       172.16.6.22
 ns3     IN      A       172.16.6.22
 
 novus   IN      A       172.16.6.22
-mattermost IN   A       172.16.6.22
-db      IN      A       172.16.6.23
-db-ro   IN      A       172.16.6.23
-redis   IN      A       172.16.6.23
-uranus  IN      A       172.16.6.24
-
-mattermost IN      A    172.16.6.23
-jenkins IN      A       172.16.6.23
+db      IN      A       172.16.6.22
+db-ro   IN      A       172.16.6.22
+redis   IN      A       172.16.6.22
+uranus  IN      A       172.16.6.22
 
 earth   IN      A       172.16.6.61
-
-
